78239a2caf0f8ca5f891205399784addbde0f84e
Osvaldo Angel <211125@ids.upchiapas.edu.mx>
2022-05-17 22:50:25 -0500

    add post

--- api/component/user/network.js
+++ api/component/user/network.js
@@ -13,7 +13,26 @@ route.get('/succes2', function(req, res) {
         success: 'success2',
     })
 })
+router.post('/login', function (req, res){
+    console.log(req.query.username);
+    console.log(req.query.password);
+    res.send({
+       token : 'token',
+       id_users: 'id_user',
+       sucess: 'valido'
+    });
+});
+router.post('/register', function (req, res) {
+    console.log(req.query.username);
+    console.log(req.query.password);
+    console.log(req.query.phoneNumber);
+    res.send({
+        token:'token',
+        id_User:'id_user',
+        success:'valido',
+    });
 
+});
 
 
 module.exports = router;
