b21c65d57a39be5e63268c6c6c8f3fc2269f3cd7
Osvaldo Angel <211125@ids.upchiapas.edu.mx>
2022-05-17 22:52:00 -0500

    Modify

--- api/component/index.js
+++ api/component/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const { status } = require('express/lib/response');
-const res = require('express/lib/response');
-
-const config = require('../config.js');
-const user = require('../components/user/network.js')
+const config =require ('../../../config.js');
+const user = require ('./network')
 
 const app = express();
 
-//ROUTERS
+
 app.use('/api/user', user)
 
-//Servidor activo
+
 app.listen(config.api.port, () => {
-    console.log('Servidor corriendo en el puerto en el puerto =>', config.api.port)
-}
-);
+    console.log(`Servidor corriendo en el puerto => ${config.api.port}` )
+});
+
